c03bf61a19e21955e01fa776412b6ca6382c80e2
Khuzin Rinat <khuzin.rinat.work@gmail.com>
2024-10-02 11:31:01 +0500

    add any components;

--- app/src/main/java/com/sample/edittextbuttononclick/MainActivity.kt
+++ app/src/main/java/com/sample/edittextbuttononclick/MainActivity.kt
@@ -1,6 +1,12 @@
 package com.sample.edittextbuttononclick
 
+import android.annotation.SuppressLint
+import android.hardware.camera2.params.OisSample
 import android.os.Bundle
+import android.text.Editable
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,7 +14,13 @@ import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var editTextSample: EditText
+    private lateinit var sampleTextResult:  TextView
+    private lateinit var countSymbolText: TextView
+    private lateinit var buttonConvert: Button
+    var stringForRevert: String = ""
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -18,7 +30,34 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        buttonConvert = findViewById(R.id.convertButton)
+        editTextSample = findViewById(R.id.editTextText)
+        sampleTextResult = findViewById(R.id.textResult)
+        countSymbolText = findViewById(R.id.symbolCounter)
+        buttonConvert.setBackgroundColor(resources.getColor(R.color.another, theme))
+
+        val beforeCount = 0
+        countSymbolText.text = "Количество символов: $beforeCount"
+
+
+        editTextSample.addTextChangedListener(object : android.text.TextWatcher{
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun onTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {}
+
+            override fun afterTextChanged(p0: Editable?) {
+                val symbolCount = p0?.length ?: 0
+                countSymbolText.text = "Количество символов: $symbolCount"
+            }
+
+        })
+        buttonConvert.setOnClickListener {
+            stringForRevert = editTextSample.text.toString()
+            sampleTextResult.text = stringForRevert.toString().reversed()
+        }
+
     }
 
-    
+
 }
